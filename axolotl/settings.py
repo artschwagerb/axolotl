a9a626fc19c290e169d34896b0bdb2bb91496b79
artschwagerb <brian@artschwager.com>
2013-09-10 20:39:30 -0400

    Seperating Settings into Local_Settings

--- axolotl/settings.py
+++ axolotl/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -124,6 +124,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'bitsync',
+    'tv',
+    'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
